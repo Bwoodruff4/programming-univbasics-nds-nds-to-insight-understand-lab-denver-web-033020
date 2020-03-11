da6387b16001fca4cb0fb23abd4839009a4d8de2
Blake <blakewoodruff4@gmail.com>
2020-03-11 22:39:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,7 +14,7 @@ def print_first_directors_movie_titles
   first_director_movies = directors_database[0][:movies]
   
   while index < first_director_movies.length do
-    puts first_director_movies[index][:titles]
+    puts first_director_movies[index][:title]
     index +=1
   end
   
